b4d05ed5219fc0977a551d5740d57d5375ee6579
p1ve7e <gabrielnascimento123@outlook.com>
2026-07-03 16:39:39 -0300

    Atualizar o variaveisqlegal.p

--- variaveisqlegal.py
+++ variaveisqlegal.py
@@ -3,4 +3,15 @@ print(f"O núMero é: {numero}.")
 
 nome = "Fulano"
 idade = 21
+
 print(f"Meu nome é {nome} e eu tenho {idade} anos.")
+
+# Usando f-string
+mensagem = f"Meu nome é {nome} e eu tenho {idade} anos."
+
+print(mensagem)  # Saída: Meu nome é Ana e eu tenho 25 anos.
+
+valor = 123.456
+
+print(f"O valor formatado é: R$ {valor:.2f}") 
+# Saída: O valor formatado é: R$ 123.46
